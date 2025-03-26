9a71ca78b67df1a23f3b1939c0d9d796c278a8ec
geethakrishna puligundla <gpuligu@clemson.edu>
2025-03-26 11:39:03 -0400

    added catalog service

---
+++ catalog-service/src/main/java/com/geethakrishna/catalog_service/domain/ProductNotFoundException.java
@@ -0,0 +1,11 @@
+package com.geethakrishna.catalog_service.domain;
+
+public class ProductNotFoundException extends RuntimeException{
+    ProductNotFoundException(String message){
+        super(message);
+    }
+
+    public static ProductNotFoundException forCode(String code){
+        return new ProductNotFoundException("Prodcut not found for code: "+ code);
+    }
+}
---
+++ catalog-service/src/test/java/com/geethakrishna/catalog_service/AbstractIT.java
@@ -0,0 +1,21 @@
+package com.geethakrishna.catalog_service;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public abstract class AbstractIT {
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    void setUp(){
+        System.out.println(port);
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+}
