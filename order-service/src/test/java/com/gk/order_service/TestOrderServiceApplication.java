6c2e69a118e653904913fc5e091531ee344c3421
geethakrishna puligundla <gpuligu@clemson.edu>
2025-03-27 15:43:23 -0400

    skeleton for the order service

---
+++ order-service/src/test/java/com/gk/order_service/TestOrderServiceApplication.java
@@ -0,0 +1,12 @@
+package com.gk.order_service;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(OrderServiceApplication::main)
+                .with(TestcontainersConfiguration.class)
+                .run(args);
+    }
+}
