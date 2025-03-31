3877f97557eec2b8c97b0397fbb03061f160f8f7
geethakrishna puligundla <gpuligu@clemson.edu>
2025-03-31 06:48:08 -0400

    added unit tests

--- order-service/src/test/java/com/gk/order_service/web/controller/OrderControllerUnitTests.java
+++ order-service/src/test/java/com/gk/order_service/web/controller/OrderControllerUnitTests.java
@@ -1,10 +1,14 @@
 package com.gk.order_service.web.controller;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gk.order_service.domain.OrderDTO;
 import com.gk.order_service.domain.OrderService;
+import com.gk.order_service.domain.OrderSummary;
 import com.gk.order_service.domain.SecurityService;
-import com.gk.order_service.domain.models.CreateOrderRequest;
+import com.gk.order_service.domain.models.*;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -13,15 +17,24 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Stream;
 
 import static com.gk.order_service.testdata.TestDataFactory.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Named.named;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -41,13 +54,13 @@ class OrderControllerUnitTests {
 
     @BeforeEach
     void setUp() {
-        given(securityService.getLoginUserName()).willReturn("gk");
+        given(securityService.getLoginUserName()).willReturn("Gk");
     }
 
     @ParameterizedTest(name = "[{index}]-{0}")
     @MethodSource("createOrderRequestProvider")
     void shouldReturnBadRequestWhenOrderPayloadIsInvalid(CreateOrderRequest request) throws Exception {
-        given(orderService.createOrder(eq("gk"), any(CreateOrderRequest.class)))
+        given(orderService.createOrder(eq("Gk"), any(CreateOrderRequest.class)))
                 .willReturn(null);
 
         mockMvc.perform(post("/api/orders")
@@ -61,5 +74,62 @@ class OrderControllerUnitTests {
                 arguments(named("Order with Invalid Customer", createOrderRequestWithInvalidCustomer())),
                 arguments(named("Order with Invalid Delivery Address", createOrderRequestWithInvalidDeliveryAddress())),
                 arguments(named("Order with No Items", createOrderRequestWithNoItems())));
+    }
+
+    @Test
+    void shouldReturnOrdersSuccessfully() throws Exception {
+        List<OrderSummary> mockOrders = List.of(
+                new OrderSummary("A101", OrderStatus.NEW),
+                new OrderSummary("A123", OrderStatus.CANCELLED)
+        );
+
+        given(orderService.getOrders("Gk")).willReturn(mockOrders);
+
+        MvcResult mvcResult = mockMvc.perform(get("/api/orders").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String jsonResponse = mvcResult.getResponse().getContentAsString();
+        List<OrderSummary> actualOrders = objectMapper.readValue(jsonResponse, new TypeReference<>() {
+        });
+
+        assertNotNull(actualOrders);
+        assertEquals(2, actualOrders.size());
+        assertEquals("A101", actualOrders.get(0).orderNumber());
+        assertEquals("A123", actualOrders.get(1).orderNumber());
+    }
+
+    @Test
+    void shouldReturnOrderDetailsWhenOrderExists() throws Exception {
+        String orderNumber = "ORD123";
+        OrderDTO mockOrder = new OrderDTO(
+                orderNumber,
+                "Gk",
+                Set.of(new OrderItem("A101", "Product A", new BigDecimal(12.22), 2)),
+                new Customer("gk", "gk@gmail.com", "86666235812"),
+                new Address("220 ELM st", "University place", "Clemson", "SC", "29631", "US"),
+                OrderStatus.NEW,
+                "Order created",
+                LocalDateTime.of(12, 12, 12, 12, 12)
+        );
+
+        given(orderService.getOrder("Gk", orderNumber)).willReturn(Optional.of(mockOrder));
+
+        MvcResult mvcResult = mockMvc.perform(get("/api/orders/{orderNumber}", orderNumber)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String jsonResponse = mvcResult.getResponse().getContentAsString();
+        OrderDTO actualOrder = objectMapper.readValue(jsonResponse, OrderDTO.class);
+
+
+        assertNotNull(actualOrder);
+        assertEquals(orderNumber, actualOrder.orderNumber());
+        assertEquals("Gk", actualOrder.user());
+        assertEquals(1, actualOrder.items().size()); // Single item in set
+        assertEquals("A101", actualOrder.items().iterator().next().code());
+        assertEquals(2, actualOrder.items().iterator().next().quantity());
+        assertEquals(new BigDecimal(12.22), actualOrder.items().iterator().next().price());
     }
 }
